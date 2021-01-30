bacc75e6abd85871c8bc3ba5ea809757da2b8a45
galihsatrio <sgalih1234@gmail.com>
2021-01-30 21:59:23 +0700

    ketiga

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -24,7 +24,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-success" style="font-family: Viga">
         <div class="container my-2">
-            <a class="navbar-brand" href="#">School</a>
+            <a class="navbar-brand" href="/">School</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -15,7 +15,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
         <div class="container my-1">
-            <a class="navbar-brand" href="#" style="font-family: viga">School</a>
+            <a class="navbar-brand" href="/" style="font-family: viga">School</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
